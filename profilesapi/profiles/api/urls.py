450f9f5b212f9aa0de65db0081a5e1b5c0315fd9
a.nurmukhambetov <a.nurmukhambetov@ensoft.kz>
2021-02-01 12:24:55 +0600

    added automation tests.

--- profilesapi/profiles/api/urls.py
+++ profilesapi/profiles/api/urls.py
@@ -6,7 +6,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r"profiles", ProfileViewSet)
-router.register(r"status", ProfileStatusViewSet)
+router.register(r"status", ProfileStatusViewSet, basename="status")
 
 urlpatterns = [
   path("",include(router.urls)),
